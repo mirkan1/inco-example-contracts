5515e6778df62a2653cff94e62fe766cace9f9cc
DevSwayam <swayamkarle_ce_2020@ltce.in>
2024-11-14 16:36:49 +0700

    Added BlindAuction Fixture

--- test/BlindAuctionTests/BlindAuction.fixture.ts
+++ test/BlindAuctionTests/BlindAuction.fixture.ts
@@ -1,18 +1,23 @@
-import { AddressLike, BigNumberish, Signer } from 'ethers';
-import { ethers } from 'hardhat';
+import { AddressLike, BigNumberish, Signer } from "ethers";
+import { ethers } from "hardhat";
 
-import type { BlindAuction } from '../../types';
+import type { BlindAuction, ConfidentialERC20 } from "../../types";
+import { getSigners } from "../signers";
 
-export async function deployBlindAuctionFixture(
-  account: Signer,
-  tokenContract: AddressLike,
-  biddingTime: BigNumberish,
-  isStoppable: boolean,
-): Promise<BlindAuction> {
-  const contractFactory = await ethers.getContractFactory('BlindAuction');
-  const contract = await contractFactory
-    .connect(account)
-    .deploy(account.getAddress(), tokenContract, biddingTime, isStoppable);
-  await contract.waitForDeployment();
-  return contract;
+export async function deployBlindAuctionFixture(): Promise<[ConfidentialERC20, BlindAuction]> {
+  const signers = await getSigners();
+
+  const ConfidentialERC20ContractFactory = await ethers.getContractFactory("ConfidentialERC20");
+  const ConfidentialERC20Contract = await ConfidentialERC20ContractFactory.connect(signers.alice).deploy();
+  await ConfidentialERC20Contract.waitForDeployment();
+
+  const BlindAuctionContractFactory = await ethers.getContractFactory("BlindAuction");
+  const BlindAuctionContract = await BlindAuctionContractFactory.connect(signers.alice).deploy(
+    signers.alice.address,
+    ConfidentialERC20Contract,
+    3600,
+    true,
+  );
+  await BlindAuctionContract.waitForDeployment();
+  return [ConfidentialERC20Contract, BlindAuctionContract];
 }
