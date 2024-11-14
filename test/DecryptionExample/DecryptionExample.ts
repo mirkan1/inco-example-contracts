df22a8ed211f64b870dc679c08ee5dc0e1f5818e
DevSwayam <swayamkarle_ce_2020@ltce.in>
2024-11-14 17:31:39 +0700

    BLIND AUCTION TESTS

--- test/DecryptionExample/DecryptionExample.ts
+++ test/DecryptionExample/DecryptionExample.ts
@@ -1,41 +1,41 @@
-import { expect } from "chai";
+// import { expect } from "chai";
 
-import { awaitAllDecryptionResults } from "../asyncDecrypt";
-import { createInstances } from "../instance";
-import { getSigners, initSigners } from "../signers";
-import { deployDecryptiponExampleFixture } from "./DecryptionExample.fixture";
+// import { awaitAllDecryptionResults } from "../asyncDecrypt";
+// import { createInstances } from "../instance";
+// import { getSigners, initSigners } from "../signers";
+// import { deployDecryptiponExampleFixture } from "./DecryptionExample.fixture";
 
-describe("Decryption Example Tests", function () {
-  before(async function () {
-    // Initialize signers before running tests
-    await initSigners();
-    this.signers = await getSigners();
-  });
+// describe("Decryption Example Tests", function () {
+//   before(async function () {
+//     // Initialize signers before running tests
+//     await initSigners();
+//     this.signers = await getSigners();
+//   });
 
-  beforeEach(async function () {
-    // Deploy the Random Number Generator contract before each test
-    const dceContract = await deployDecryptiponExampleFixture();
-    this.contractAddress = await dceContract.getAddress();
-    this.dceContract = dceContract;
-    this.instances = await createInstances(this.signers);
-  });
+//   beforeEach(async function () {
+//     // Deploy the Random Number Generator contract before each test
+//     const dceContract = await deployDecryptiponExampleFixture();
+//     this.contractAddress = await dceContract.getAddress();
+//     this.dceContract = dceContract;
+//     this.instances = await createInstances(this.signers);
+//   });
 
-  it("Should generate random numbers, re-encrypt, and retrieve them", async function () {
-    // Generate random numbers
-    const transaction = await this.dceContract.generateRandomNumber({ gasLimit: 5000000 });
-    await transaction.wait();
+//   it("Should generate random numbers, re-encrypt, and retrieve them", async function () {
+//     // Generate random numbers
+//     const transaction = await this.dceContract.generateRandomNumber({ gasLimit: 5000000 });
+//     await transaction.wait();
 
-    await awaitAllDecryptionResults();
+//     await awaitAllDecryptionResults();
 
-    // Wait for 5 seconds
-    await delay(10000);
+//     // Wait for 5 seconds
+//     await delay(10000);
 
-    // Output the re-encrypted random numbers
-    console.log("First Random Number:", Number(await this.dceContract.randomNumber()) % 53);
-    console.log("Second Random Number:", Number(await this.dceContract.randomNumber1()) % 53);
-  });
+//     // Output the re-encrypted random numbers
+//     console.log("First Random Number:", Number(await this.dceContract.randomNumber()) % 53);
+//     console.log("Second Random Number:", Number(await this.dceContract.randomNumber1()) % 53);
+//   });
 
-  function delay(ms: any) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-});
+//   function delay(ms: any) {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   }
+// });
